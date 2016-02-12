5cbb007ce032c983810c31ddcc078c30e0355bed
Nestor <nestor.2005@gmail.com>
2016-02-12 03:51:43 -0300

    [FIX] demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,12 @@
 angular.module('demoApp', [
    "electron-draggable-bar"
 ])
-.controller('demoCtrl', function($scope) {
+.controller('demoCtrl', function($scope, $window) {
    $scope.showAlert = function() {
       alert('Soy un alert!');
    };
+
+   $scope.exitWindow = function() {
+      $window.close();
+   }
 });
